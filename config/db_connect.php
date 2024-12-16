5cb87eb5fbe239f2869732196cfd8ea7335f3f42
Lady-Margaret (Marge) Hagan <144915421+ladymargehagan@users.noreply.github.com>
2024-12-16 22:22:55 +0000

    final db

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 try {
     $pdo = new PDO(
-        "mysql:host=localhost;dbname=litdb",
+        "mysql:host=localhost;dbname=webtech_fall2024_lady_hagan",
         "root",
         "",
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
